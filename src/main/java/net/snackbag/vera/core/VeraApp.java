84b9db515d3809ee587ce4e695836d6e6737a586
JXSnack <jx.snack@gmail.com>
2024-10-29 00:09:56 +0100

    handle widgets

--- src/main/java/net/snackbag/vera/core/VeraApp.java
+++ src/main/java/net/snackbag/vera/core/VeraApp.java
@@ -1,13 +1,33 @@
 package net.snackbag.vera.core;
 
 import net.snackbag.vera.VeraProvider;
+import net.snackbag.vera.widget.VWidget;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class VeraApp {
     private final VeraProvider provider;
+    private final List<VWidget> widgets;
 
     public VeraApp(VeraProvider provider) {
         this.provider = provider;
+        this.widgets = new ArrayList<>();
+
+        provider.handleAppInitialization(this);
+    }
+
+    public VeraProvider getProvider() {
+        return provider;
     }
 
     public abstract void init();
+
+    public List<VWidget> getWidgets() {
+        return new ArrayList<>(widgets);
+    }
+
+    public void addWidget(VWidget widget) {
+        this.widgets.add(widget);
+    }
 }
