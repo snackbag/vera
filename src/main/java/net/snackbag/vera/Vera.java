3a6dcdf3d1e7f7f5a1a3bf86b5c77a50b8ac2d9a
JXSnack <jx.snack@gmail.com>
2024-10-29 00:02:47 +0100

    add getProvider

--- src/main/java/net/snackbag/vera/Vera.java
+++ src/main/java/net/snackbag/vera/Vera.java
@@ -5,11 +5,16 @@ import java.util.HashMap;
 public class Vera {
     private static final HashMap<String, VeraProvider> providers = new HashMap<>();
 
-    public void registerProvider(String name, VeraProvider provider) {
+    public static void registerProvider(String name, VeraProvider provider) {
         if (providers.containsKey(name)) {
             throw new UnsupportedOperationException("There is already a provider named '" + name + "' registered");
         }
 
         providers.put(name, provider);
+    }
+
+    public static VeraProvider getProvider(String name) {
+        if (providers.containsKey(name)) return providers.get(name);
+        throw new NullPointerException("Unknown UI provider '" + name + "'");
     }
 }
