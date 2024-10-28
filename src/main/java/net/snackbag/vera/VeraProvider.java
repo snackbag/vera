88ae030330a24526095f518691eb59e0453093dd
JXSnack <jx.snack@gmail.com>
2024-10-29 00:16:21 +0100

    add renderLabel method

--- src/main/java/net/snackbag/vera/VeraProvider.java
+++ src/main/java/net/snackbag/vera/VeraProvider.java
@@ -1,7 +1,10 @@
 package net.snackbag.vera;
 
 import net.snackbag.vera.core.VeraApp;
+import net.snackbag.vera.widget.VLabel;
 
 public interface VeraProvider {
     void handleAppInitialization(VeraApp app);
+
+    void renderLabel(VLabel label);
 }
