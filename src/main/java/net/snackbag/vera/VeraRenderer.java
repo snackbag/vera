44084320a3580cc43454157d96b517ec8180caee
JXSnack <jx.snack@gmail.com>
2024-10-29 08:57:13 +0100

    rect, just maybe, might also need a color

--- src/main/java/net/snackbag/vera/VeraRenderer.java
+++ src/main/java/net/snackbag/vera/VeraRenderer.java
@@ -1,9 +1,10 @@
 package net.snackbag.vera;
 
+import net.snackbag.vera.core.VColor;
 import net.snackbag.vera.core.VFont;
 
 public interface VeraRenderer {
-    void drawRect(int x, int y, int width, int height);
+    void drawRect(int x, int y, int width, int height, VColor color);
 
     void drawText(int x, int y, VFont font);
 }
