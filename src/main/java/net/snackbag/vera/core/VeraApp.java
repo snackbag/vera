5739e4cb973479fbc6afbccf8bce9e12858f2adc
JXSnack <jx.snack@gmail.com>
2024-10-29 08:25:57 +0100

    background color

--- src/main/java/net/snackbag/vera/core/VeraApp.java
+++ src/main/java/net/snackbag/vera/core/VeraApp.java
@@ -9,10 +9,12 @@ import java.util.List;
 public abstract class VeraApp {
     private final VeraProvider provider;
     private final List<VWidget> widgets;
+    private VColor backgroundColor;
 
     public VeraApp(VeraProvider provider) {
         this.provider = provider;
         this.widgets = new ArrayList<>();
+        this.backgroundColor = VColor.white();
 
         provider.handleAppInitialization(this);
     }
@@ -29,5 +31,13 @@ public abstract class VeraApp {
 
     public void addWidget(VWidget widget) {
         this.widgets.add(widget);
+    }
+
+    public void setBackgroundColor(VColor color) {
+        this.backgroundColor = color;
+    }
+
+    public VColor getBackgroundColor() {
+        return backgroundColor;
     }
 }
