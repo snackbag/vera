d58c8f118f5ba4ff70ac82fddcdcee2bf49694b2
JXSnack <jx.snack@gmail.com>
2024-10-29 08:37:51 +0100

    rename to renderApp

--- src/main/java/net/snackbag/vera/VeraProvider.java
+++ src/main/java/net/snackbag/vera/VeraProvider.java
@@ -1,12 +1,11 @@
 package net.snackbag.vera;
 
 import net.snackbag.vera.core.VeraApp;
-import net.snackbag.vera.widget.VLabel;
 
 public interface VeraProvider {
     void handleAppInitialization(VeraApp app);
 
-    void renderLabel(VLabel label);
+    void renderApp(VeraApp app);
 
     int getScreenHeight();
 
