c302822cfd3952c8f87285f3991de6d5f6cf14f5
JXSnack <jx.snack@gmail.com>
2024-10-29 09:54:59 +0100

    vera data

---
+++ src/main/java/net/snackbag/mcvera/MCVeraData.java
@@ -0,0 +1,11 @@
+package net.snackbag.mcvera;
+
+import net.snackbag.vera.core.VeraApp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MCVeraData {
+    public static List<VeraApp> applications = new ArrayList<>();
+    public static List<VeraApp> visibleApplications = new ArrayList<>();
+}
