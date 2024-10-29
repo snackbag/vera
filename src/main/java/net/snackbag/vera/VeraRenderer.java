7279ca80dd0ba5b1fc13d141cd5e74184d362172
JXSnack <jx.snack@gmail.com>
2024-10-29 11:27:09 +0100

    add rotation argument

--- src/main/java/net/snackbag/vera/VeraRenderer.java
+++ src/main/java/net/snackbag/vera/VeraRenderer.java
@@ -5,11 +5,11 @@ import net.snackbag.vera.core.VFont;
 import net.snackbag.vera.core.VeraApp;
 
 public interface VeraRenderer {
-    void drawRect(VeraApp app, int x, int y, int width, int height, VColor color);
+    void drawRect(VeraApp app, int x, int y, int width, int height, double rotation, VColor color);
 
-    void drawText(VeraApp app, int x, int y, String text, VFont font);
+    void drawText(VeraApp app, int x, int y, double rotation, String text, VFont font);
 
     void renderApp(VeraApp app);
 
-    void drawImage(VeraApp app, int x, int y, int width, int height, String path);
+    void drawImage(VeraApp app, int x, int y, int width, int height, double rotation, String path);
 }
