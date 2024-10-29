33c799f8f846f3d60d29f9f9b785a191cf6e8073
JXSnack <jx.snack@gmail.com>
2024-10-29 11:25:50 +0100

    add images!

---
+++ src/main/java/net/snackbag/vera/widget/VImage.java
@@ -0,0 +1,29 @@
+package net.snackbag.vera.widget;
+
+import net.snackbag.vera.VeraRenderer;
+import net.snackbag.vera.core.VeraApp;
+
+public class VImage extends VWidget {
+    private String path;
+
+    public VImage(String path, int width, int height, VeraApp app) {
+        super(0, 0, width, height, app);
+
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public void render() {
+        VeraRenderer renderer = getRenderer();
+
+        renderer.drawImage(app, x, y, width, height, path);
+    }
+}
