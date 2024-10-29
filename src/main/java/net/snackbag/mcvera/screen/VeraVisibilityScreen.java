807d5076b98fc32b9539b9b33baf50d0b77b7f89
JXSnack <jx.snack@gmail.com>
2024-10-29 21:01:48 +0100

    click events

--- src/main/java/net/snackbag/mcvera/screen/VeraVisibilityScreen.java
+++ src/main/java/net/snackbag/mcvera/screen/VeraVisibilityScreen.java
@@ -2,6 +2,9 @@ package net.snackbag.mcvera.screen;
 
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
+import net.snackbag.mcvera.MCVeraData;
+import net.snackbag.vera.core.VeraApp;
+import net.snackbag.vera.widget.VWidget;
 
 /**
  * Existence is required for mouse movements
@@ -19,5 +22,21 @@ public class VeraVisibilityScreen extends Screen {
     @Override
     public boolean shouldPause() {
         return false;
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        for (VeraApp app : MCVeraData.visibleApplications) {
+            for (VWidget widget : app.getHoveredWidgets((int) mouseX, (int) mouseY)) {
+                switch (button) {
+                    case 0: widget.fireEvent("left-click");
+                    case 1: widget.fireEvent("right-click");
+                    case 2: widget.fireEvent("middle-click");
+                    default: throw new IllegalStateException("Invalid button type: " + button);
+                }
+            }
+        }
+
+        return true;
     }
 }
