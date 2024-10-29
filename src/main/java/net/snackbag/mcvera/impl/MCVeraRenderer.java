48980b5cdf176dc566a1d74664fdabbe5d991bf7
JXSnack <jx.snack@gmail.com>
2024-10-29 10:24:40 +0100

    render rectangles and text

--- src/main/java/net/snackbag/mcvera/impl/MCVeraRenderer.java
+++ src/main/java/net/snackbag/mcvera/impl/MCVeraRenderer.java
@@ -1,12 +1,18 @@
 package net.snackbag.mcvera.impl;
 
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
 import net.snackbag.vera.VeraRenderer;
 import net.snackbag.vera.core.VColor;
 import net.snackbag.vera.core.VFont;
 import net.snackbag.vera.core.VeraApp;
+import net.snackbag.vera.widget.VWidget;
+
+import java.util.List;
 
 public class MCVeraRenderer implements VeraRenderer {
     private static MCVeraRenderer instance = null;
+    public static DrawContext drawContext = null;
 
     public static MCVeraRenderer getInstance() {
         if (instance == null) {
@@ -18,16 +24,20 @@ public class MCVeraRenderer implements VeraRenderer {
 
     @Override
     public void drawRect(VeraApp app, int x, int y, int width, int height, VColor color) {
-
+        drawContext.fill(x, y, width, height, color.toInt());
     }
 
     @Override
     public void drawText(VeraApp app, int x, int y, String text, VFont font) {
-
+        drawContext.drawText(MinecraftClient.getInstance().textRenderer, text, x, y, font.getColor().toInt(), true);
     }
 
     @Override
     public void renderApp(VeraApp app) {
+        List<VWidget> widgets = app.getWidgets();
 
+        for (VWidget widget : widgets) {
+            widget.render();
+        }
     }
 }
