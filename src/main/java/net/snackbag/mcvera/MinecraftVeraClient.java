f23ea6306d2bb930483f66a44eb423e673ec75a1
JXSnack <jx.snack@gmail.com>
2024-10-29 17:53:19 +0100

    fix rendering (somehow)

--- src/main/java/net/snackbag/mcvera/MinecraftVeraClient.java
+++ src/main/java/net/snackbag/mcvera/MinecraftVeraClient.java
@@ -1,9 +1,12 @@
 package net.snackbag.mcvera;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.snackbag.mcvera.impl.MCVeraProvider;
+import net.snackbag.mcvera.impl.MCVeraRenderer;
 import net.snackbag.mcvera.test.TestHandler;
 import net.snackbag.vera.Vera;
+import net.snackbag.vera.core.VeraApp;
 
 public class MinecraftVeraClient implements ClientModInitializer {
     @Override
@@ -12,5 +15,14 @@ public class MinecraftVeraClient implements ClientModInitializer {
 
         Vera.registerProvider("mcvera", new MCVeraProvider());
         TestHandler.impl(false);
+
+        HudRenderCallback.EVENT.register((context, f) -> {
+            MCVeraRenderer.drawContext = context;
+            MCVeraRenderer renderer = MCVeraRenderer.getInstance();
+
+            for (VeraApp app : MCVeraData.visibleApplications) {
+                renderer.renderApp(app);
+            }
+        });
     }
 }
