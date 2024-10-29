10e6ab93731609d5026fcbcc77449d5bfebed4c3
JXSnack <jx.snack@gmail.com>
2024-10-29 09:43:49 +0100

    now require VeraApp

--- src/main/java/net/snackbag/vera/VeraRenderer.java
+++ src/main/java/net/snackbag/vera/VeraRenderer.java
@@ -2,9 +2,10 @@ package net.snackbag.vera;
 
 import net.snackbag.vera.core.VColor;
 import net.snackbag.vera.core.VFont;
+import net.snackbag.vera.core.VeraApp;
 
 public interface VeraRenderer {
-    void drawRect(int x, int y, int width, int height, VColor color);
+    void drawRect(VeraApp app, int x, int y, int width, int height, VColor color);
 
-    void drawText(int x, int y, String text, VFont font);
+    void drawText(VeraApp app, int x, int y, String text, VFont font);
 }
