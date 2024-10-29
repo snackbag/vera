17d1d2bc8255846aa0dbe004c5c39c22d94b4021
JXSnack <jx.snack@gmail.com>
2024-10-29 08:53:38 +0100

    padding methods

--- src/main/java/net/snackbag/vera/widget/VLabel.java
+++ src/main/java/net/snackbag/vera/widget/VLabel.java
@@ -1,6 +1,7 @@
 package net.snackbag.vera.widget;
 
 import net.snackbag.vera.VeraRenderer;
+import net.snackbag.vera.core.V4Int;
 import net.snackbag.vera.core.VColor;
 import net.snackbag.vera.core.VeraApp;
 
@@ -8,6 +9,7 @@ public class VLabel extends VWidget {
     private String text;
     private int fontSize;
     private VColor backgroundColor;
+    private V4Int padding;
 
     public VLabel(String text, VeraApp app) {
         super(0, 0, 100, 12, app);
@@ -15,6 +17,7 @@ public class VLabel extends VWidget {
         this.text = text;
         this.fontSize = 16;
         this.backgroundColor = VColor.transparent();
+        this.padding = new V4Int(4);
     }
 
     public String getText() {
@@ -39,6 +42,26 @@ public class VLabel extends VWidget {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public V4Int getPadding() {
+        return padding;
+    }
+
+    public void setPadding(V4Int padding) {
+        this.padding = padding;
+    }
+
+    public void setPadding(int all) {
+        setPadding(new V4Int(all));
+    }
+
+    public void setPadding(int tb, int lr) {
+        setPadding(new V4Int(tb, lr));
+    }
+
+    public void setPadding(int top, int bottom, int left, int right) {
+        setPadding(new V4Int(top, bottom, left, right));
     }
 
     @Override
