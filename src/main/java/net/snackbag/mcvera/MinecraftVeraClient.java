04e10c08931d7b7fdb462573a359ded72f74b5ad
JXSnack <jx.snack@gmail.com>
2024-10-29 09:29:51 +0100

    register the provider

--- src/main/java/net/snackbag/mcvera/MinecraftVeraClient.java
+++ src/main/java/net/snackbag/mcvera/MinecraftVeraClient.java
@@ -1,10 +1,14 @@
 package net.snackbag.mcvera;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.snackbag.mcvera.impl.MCVeraProvider;
+import net.snackbag.vera.Vera;
 
 public class MinecraftVeraClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         MinecraftVera.LOGGER.info("Loading client Vera implementation...");
+
+        Vera.registerProvider("mcvera", new MCVeraProvider());
     }
 }
