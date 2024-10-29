ac457af4d810987dee8577b6ed2020a698d1b083
JXSnack <jx.snack@gmail.com>
2024-10-29 10:35:17 +0100

    fix keybind (again)

--- src/main/java/net/snackbag/mcvera/test/TestHandler.java
+++ src/main/java/net/snackbag/mcvera/test/TestHandler.java
@@ -3,7 +3,9 @@ package net.snackbag.mcvera.test;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public class TestHandler {
@@ -22,7 +24,7 @@ public class TestHandler {
         KeyBindingHelper.registerKeyBinding(testKeybinding);
 
         ClientTickEvents.END_CLIENT_TICK.register((client) -> {
-            while (testKeybinding.wasPressed()) {
+            if (InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), GLFW.GLFW_KEY_APOSTROPHE)) {
                 TestApplication.INSTANCE.setVisibility(!TestApplication.INSTANCE.isVisible());
             }
         });
