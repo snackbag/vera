32419972fe2c0dae9c57a8d5b69df719498c544e
JXSnack <jx.snack@gmail.com>
2024-10-29 09:55:04 +0100

    render loop

--- src/main/java/net/snackbag/mcvera/MinecraftVeraClient.java
+++ src/main/java/net/snackbag/mcvera/MinecraftVeraClient.java
@@ -1,9 +1,12 @@
 package net.snackbag.mcvera;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.snackbag.mcvera.impl.MCVeraProvider;
+import net.snackbag.mcvera.impl.MCVeraRenderer;
 import net.snackbag.mcvera.test.TestHandler;
 import net.snackbag.vera.Vera;
+import net.snackbag.vera.core.VeraApp;
 
 public class MinecraftVeraClient implements ClientModInitializer {
     @Override
@@ -12,5 +15,15 @@ public class MinecraftVeraClient implements ClientModInitializer {
 
         Vera.registerProvider("mcvera", new MCVeraProvider());
         TestHandler.impl(false);
+
+        ClientTickEvents.END_CLIENT_TICK.register((client) -> renderApplications());
+    }
+
+    public void renderApplications() {
+        MCVeraRenderer renderer = MCVeraRenderer.getInstance();
+
+        for (VeraApp app : MCVeraData.visibleApplications) {
+            renderer.renderApp(app);
+        }
     }
 }
