2b82edaa330155c274988fcdb1d9db1c9c98637c
JXSnack <jx.snack@gmail.com>
2024-10-29 10:01:35 +0100

    add VeraVisibilityScreen

---
+++ src/main/java/net/snackbag/mcvera/screen/VeraVisibilityScreen.java
@@ -0,0 +1,13 @@
+package net.snackbag.mcvera.screen;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+/**
+ * Existence is required for mouse movements
+ */
+public class VeraVisibilityScreen extends Screen {
+    public VeraVisibilityScreen() {
+        super(Text.of("Vera"));
+    }
+}
