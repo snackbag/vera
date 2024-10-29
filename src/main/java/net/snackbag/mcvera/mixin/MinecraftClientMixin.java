d19e0d815de0edd7560d1b1c53601228ebe0e29d
JXSnack <jx.snack@gmail.com>
2024-10-29 10:01:29 +0100

    yes

---
+++ src/main/java/net/snackbag/mcvera/mixin/MinecraftClientMixin.java
@@ -0,0 +1,24 @@
+package net.snackbag.mcvera.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.snackbag.mcvera.MCVeraData;
+import net.snackbag.mcvera.screen.VeraVisibilityScreen;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public abstract class MinecraftClientMixin {
+	@Shadow public abstract void setScreen(@Nullable Screen screen);
+
+	@Inject(at = @At("HEAD"), method = "setScreen")
+	private void init(@Nullable Screen screen, CallbackInfo ci) {
+		if (screen == null && !MCVeraData.visibleApplications.isEmpty()) {
+			setScreen(new VeraVisibilityScreen());
+		}
+	}
+}
