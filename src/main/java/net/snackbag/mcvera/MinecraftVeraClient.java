a4b434c987c416c6ba10369aeaa6c4b73dc42da3
JXSnack <jx.snack@gmail.com>
2024-10-30 23:48:07 +0100

    key ahndling

--- src/main/java/net/snackbag/mcvera/MinecraftVeraClient.java
+++ src/main/java/net/snackbag/mcvera/MinecraftVeraClient.java
@@ -1,14 +1,22 @@
 package net.snackbag.mcvera;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.InputUtil;
 import net.snackbag.mcvera.impl.MCVeraProvider;
 import net.snackbag.mcvera.impl.MCVeraRenderer;
 import net.snackbag.mcvera.test.TestHandler;
 import net.snackbag.vera.Vera;
 import net.snackbag.vera.core.VeraApp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MinecraftVeraClient implements ClientModInitializer {
+    public final static List<Integer> pressedKeys = new ArrayList<>();
+
     @Override
     public void onInitializeClient() {
         MinecraftVera.LOGGER.info("Loading client Vera implementation...");
@@ -24,5 +32,27 @@ public class MinecraftVeraClient implements ClientModInitializer {
                 renderer.renderApp(app);
             }
         });
+
+        ClientTickEvents.END_CLIENT_TICK.register((client) -> {
+            String combination = makeCombination();
+
+            for (VeraApp app : MCVeraData.visibleApplications) {
+                app.handleShortcut(combination);
+            }
+        });
+    }
+
+    private String makeCombination() {
+        StringBuilder builder = new StringBuilder();
+
+        for (int key : pressedKeys) {
+            builder
+                    .append(InputUtil.fromKeyCode(key, 0).getLocalizedText().getString().toLowerCase().replace(" ", ""))
+                    .append("+");
+        }
+
+        if (builder.length() > 1) builder.deleteCharAt(builder.length() - 1);
+
+        return builder.toString();
     }
 }
