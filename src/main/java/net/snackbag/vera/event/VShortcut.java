ccff7409565111fda849cb7b242538d68f46c162
JXSnack <jx.snack@gmail.com>
2024-10-30 23:09:02 +0100

    shortcut class

---
+++ src/main/java/net/snackbag/vera/event/VShortcut.java
@@ -0,0 +1,35 @@
+package net.snackbag.vera.event;
+
+import net.snackbag.vera.core.VeraApp;
+
+public class VShortcut {
+    private final VeraApp app;
+    private final String combination;
+    private Runnable event;
+
+    public VShortcut(VeraApp app, String combination, Runnable event) {
+        this.app = app;
+        this.combination = combination;
+        this.event = event;
+    }
+
+    public VeraApp getApp() {
+        return app;
+    }
+
+    public String getCombination() {
+        return combination;
+    }
+
+    public Runnable getEvent() {
+        return event;
+    }
+
+    public void setEvent(Runnable event) {
+        this.event = event;
+    }
+
+    public void run() {
+        app.getProvider().handleShortcut(this);
+    }
+}
