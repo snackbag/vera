99592be9f9dcae9f5065f7e6cbb9151b59e06993
JXSnack <jx.snack@gmail.com>
2024-10-31 11:47:23 +0100

    make abstract

--- src/main/java/net/snackbag/mcvera/mixin/KeyboardMixin.java
+++ src/main/java/net/snackbag/mcvera/mixin/KeyboardMixin.java
@@ -2,14 +2,13 @@ package net.snackbag.mcvera.mixin;
 
 import net.minecraft.client.Keyboard;
 import net.snackbag.mcvera.MCVeraData;
-import net.snackbag.mcvera.MinecraftVeraClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Keyboard.class)
-public class KeyboardMixin {
+public abstract class KeyboardMixin {
     @Inject(at = @At("HEAD"), method = "onKey")
     private void mcvera$handleKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
         if (action == 1) MCVeraData.pressedKeys.add(key);
