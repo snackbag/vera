d8fa3154b7ebfa13708328f38a728222e8fba4a0
JXSnack <jx.snack@gmail.com>
2024-10-31 11:44:51 +0100

    map keys

--- src/main/java/net/snackbag/mcvera/MinecraftVeraClient.java
+++ src/main/java/net/snackbag/mcvera/MinecraftVeraClient.java
@@ -1,16 +1,22 @@
 package net.snackbag.mcvera;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.resource.Resource;
+import net.minecraft.util.Identifier;
 import net.snackbag.mcvera.impl.MCVeraProvider;
 import net.snackbag.mcvera.impl.MCVeraRenderer;
 import net.snackbag.mcvera.test.TestHandler;
 import net.snackbag.vera.Vera;
 import net.snackbag.vera.core.VeraApp;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,7 +40,7 @@ public class MinecraftVeraClient implements ClientModInitializer {
         });
 
         ClientTickEvents.END_CLIENT_TICK.register((client) -> {
-            String combination = makeCombination();
+            String combination = makeCombination(client);
 
             for (VeraApp app : MCVeraData.visibleApplications) {
                 app.handleShortcut(combination);
@@ -42,13 +48,26 @@ public class MinecraftVeraClient implements ClientModInitializer {
         });
     }
 
-    private String makeCombination() {
+    private String makeCombination(MinecraftClient client) {
         StringBuilder builder = new StringBuilder();
+        Resource resource = client.getResourceManager().getResource(new Identifier(MinecraftVera.MOD_ID, "key_mappings.json")).orElseThrow();
+        JsonObject json;
+
+        try {
+            InputStreamReader reader = new InputStreamReader(resource.getInputStream());
+            json = new Gson().fromJson(reader, JsonObject.class);
+            reader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         for (int key : pressedKeys) {
-            builder
-                    .append(InputUtil.fromKeyCode(key, 0).getLocalizedText().getString().toLowerCase().replace(" ", ""))
-                    .append("+");
+            String translationKey = InputUtil.fromKeyCode(key, 0).getTranslationKey().toLowerCase().replace(" ", "");
+            String mapped = json.has(translationKey) ?
+                    json.get(translationKey).getAsString().toLowerCase().replace(" ", "") :
+                    translationKey.replace("key.keyboard.", "");
+
+            builder.append(mapped).append("+");
         }
 
         if (builder.length() > 1) builder.deleteCharAt(builder.length() - 1);
