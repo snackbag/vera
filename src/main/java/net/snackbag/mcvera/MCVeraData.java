a836d9c2b44dcb015bff049b05f6c6f97f51e0ff
JXSnack <jx.snack@gmail.com>
2024-10-31 18:58:25 +0100

    fix double keybinds

--- src/main/java/net/snackbag/mcvera/MCVeraData.java
+++ src/main/java/net/snackbag/mcvera/MCVeraData.java
@@ -8,9 +8,8 @@ import java.util.List;
 import java.util.Set;
 
 public class MCVeraData {
-    public static List<VeraApp> applications = new ArrayList<>();
-    public static List<VeraApp> visibleApplications = new ArrayList<>();
-    public final static List<Integer> pressedKeys = new ArrayList<>();
+    public static Set<VeraApp> applications = new HashSet<>();
+    public static Set<VeraApp> visibleApplications = new HashSet<>();
     public static final List<Integer> pressedKeys = new ArrayList<>();
     public static List<Integer> previousPressedKeys = new ArrayList<>();
     public static final Set<VeraApp> debugApps = new HashSet<>();
