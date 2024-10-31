568e0ffd32b32b3e64e18a45ab2dbd6321c50182
JXSnack <jx.snack@gmail.com>
2024-10-31 22:18:11 +0100

    transform osx

--- src/main/java/net/snackbag/vera/event/VShortcut.java
+++ src/main/java/net/snackbag/vera/event/VShortcut.java
@@ -1,16 +1,23 @@
 package net.snackbag.vera.event;
 
 import net.snackbag.vera.core.VeraApp;
+import org.apache.commons.lang3.SystemUtils;
 
 public class VShortcut {
     private final VeraApp app;
     private final String combination;
+    private final boolean transformOSX;
     private Runnable event;
 
     public VShortcut(VeraApp app, String combination, Runnable event) {
+        this(app, combination, event, true);
+    }
+
+    public VShortcut(VeraApp app, String combination, Runnable event, boolean transformOSX) {
         this.app = app;
         this.combination = combination.toLowerCase().replace(" ", "");
         this.event = event;
+        this.transformOSX = transformOSX;
     }
 
     public VeraApp getApp() {
@@ -18,7 +25,15 @@ public class VShortcut {
     }
 
     public String getCombination() {
-        return combination;
+        return applyTransformations(transformOSX, combination);
+    }
+
+    public static String applyTransformations(boolean transformOSX, String combination) {
+        return transformOSX && SystemUtils.IS_OS_MAC_OSX ?
+                combination
+                        .replace("leftctrl", "leftmeta")
+                        .replace("rightctrl", "rightmeta")
+                : combination;
     }
 
     public Runnable getEvent() {
@@ -27,6 +42,10 @@ public class VShortcut {
 
     public void setEvent(Runnable event) {
         this.event = event;
+    }
+
+    public boolean shouldTransformOSX() {
+        return transformOSX;
     }
 
     public void run() {
