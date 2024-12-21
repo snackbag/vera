94f0a3930036446bc6326e144b92e5a35c5ea628
JXSnack <jx.snack@gmail.com>
2024-12-21 21:18:36 +0100

    add forHoveredWidget method

--- src/main/java/net/snackbag/vera/Vera.java
+++ src/main/java/net/snackbag/vera/Vera.java
@@ -1,7 +1,13 @@
 package net.snackbag.vera;
 
+import net.snackbag.mcvera.MCVeraData;
 import net.snackbag.mcvera.impl.MCVeraProvider;
 import net.snackbag.mcvera.impl.MCVeraRenderer;
+import net.snackbag.vera.core.VeraApp;
+import net.snackbag.vera.widget.VWidget;
+
+import java.util.List;
+import java.util.function.Consumer;
 
 public class Vera {
     public static final MCVeraProvider provider = new MCVeraProvider();
@@ -9,4 +15,15 @@ public class Vera {
 
     public static final String FONT_DEFAULT = provider.getDefaultFontName();
     public static final String FONT_ARIAL = "minecraft:arial";
+
+    public static void forHoveredWidget(int mouseX, int mouseY, Consumer<VWidget<?>> runnable) {
+        for (VeraApp app : MCVeraData.visibleApplications) {
+            List<VWidget<?>> hoveredWidgets = app.getHoveredWidgets(mouseX, mouseY);
+            if (hoveredWidgets.isEmpty()) return;
+
+            for (VWidget<?> widget : hoveredWidgets) {
+                runnable.accept(widget);
+            }
+        }
+    }
 }
