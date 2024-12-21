5bc034a256043c9f512a257c35d8db161423f221
JXSnack <jx.snack@gmail.com>
2024-12-21 21:20:55 +0100

    remove redundant list import

--- src/main/java/net/snackbag/mcvera/mixin/ParentElementMixin.java
+++ src/main/java/net/snackbag/mcvera/mixin/ParentElementMixin.java
@@ -7,8 +7,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.List;
-
 @Mixin(ParentElement.class)
 public interface ParentElementMixin {
     @Inject(method = "mouseClicked", at = @At("HEAD"))
