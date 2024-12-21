1044a3618c17d4929875ddda978c58e5fdd67d5c
JXSnack <jx.snack@gmail.com>
2024-12-21 20:35:14 +0100

    create font modifier

--- src/main/java/net/snackbag/vera/core/VFont.java
+++ src/main/java/net/snackbag/vera/core/VFont.java
@@ -2,6 +2,8 @@ package net.snackbag.vera.core;
 
 import net.snackbag.vera.Vera;
 
+import java.util.function.Consumer;
+
 public class VFont {
     private final String name;
     private final int size;
@@ -47,5 +49,57 @@ public class VFont {
 
     public VFont withName(String name) {
         return new VFont(name, size, color);
+    }
+
+    public static class FontModifier {
+        private VFont font;
+        private final Consumer<VFont> fontUpdater;
+
+        public FontModifier(VFont font, Consumer<VFont> fontUpdater) {
+            this.font = font;
+            this.fontUpdater = fontUpdater;
+        }
+
+        public FontModifier color(VColor color) {
+            font = font.withColor(color);
+            fontUpdater.accept(font);
+            return this;
+        }
+
+        public FontModifier colorRed(int red) {
+            font = font.withColor(font.getColor().withRed(red));
+            fontUpdater.accept(font);
+            return this;
+        }
+
+        public FontModifier colorGreen(int green) {
+            font = font.withColor(font.getColor().withGreen(green));
+            fontUpdater.accept(font);
+            return this;
+        }
+
+        public FontModifier colorBlue(int blue) {
+            font = font.withColor(font.getColor().withBlue(blue));
+            fontUpdater.accept(font);
+            return this;
+        }
+
+        public FontModifier colorOpacity(float opacity) {
+            font = font.withColor(font.getColor().withOpacity(opacity));
+            fontUpdater.accept(font);
+            return this;
+        }
+
+        public FontModifier name(String name) {
+            font = font.withName(name);
+            fontUpdater.accept(font);
+            return this;
+        }
+
+        public FontModifier size(int size) {
+            font = font.withSize(size);
+            fontUpdater.accept(font);
+            return this;
+        }
     }
 }
