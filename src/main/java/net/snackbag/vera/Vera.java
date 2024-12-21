d861aa5fb8a7eb2c18df1e21abc04f3d51ee9ae1
JXSnack <jx.snack@gmail.com>
2024-12-21 17:15:34 +0100

    add static final methods for fonts

--- src/main/java/net/snackbag/vera/Vera.java
+++ src/main/java/net/snackbag/vera/Vera.java
@@ -6,4 +6,7 @@ import net.snackbag.mcvera.impl.MCVeraRenderer;
 public class Vera {
     public static final MCVeraProvider provider = new MCVeraProvider();
     public static final MCVeraRenderer renderer = new MCVeraRenderer();
+
+    public static final String FONT_DEFAULT = provider.getDefaultFontName();
+    public static final String FONT_ARIAL = "minecraft:arial";
 }
