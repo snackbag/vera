020b955a11d2db9e6dc159dc88705ef831b2d752
JXSnack <jx.snack@gmail.com>
2024-12-22 11:54:01 +0100

    better handling

--- src/main/java/net/snackbag/vera/Vera.java
+++ src/main/java/net/snackbag/vera/Vera.java
@@ -5,6 +5,7 @@ import net.snackbag.mcvera.impl.MCVeraProvider;
 import net.snackbag.mcvera.impl.MCVeraRenderer;
 import net.snackbag.vera.core.VeraApp;
 import net.snackbag.vera.widget.VWidget;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.function.Consumer;
@@ -16,14 +17,28 @@ public class Vera {
     public static final String FONT_DEFAULT = provider.getDefaultFontName();
     public static final String FONT_ARIAL = "minecraft:arial";
 
-    public static void forHoveredWidget(int mouseX, int mouseY, Consumer<VWidget<?>> runnable) {
+    public static void forHoveredWidget(int mouseX, int mouseY, Consumer<VWidget<?>> runnable, @Nullable Consumer<VeraApp> ifEmpty) {
         for (VeraApp app : MCVeraData.visibleApplications) {
             List<VWidget<?>> hoveredWidgets = app.getHoveredWidgets(mouseX, mouseY);
-            if (hoveredWidgets.isEmpty()) return;
+            if (hoveredWidgets.isEmpty()) {
+                if (ifEmpty != null) ifEmpty.accept(app);
+                return;
+            }
 
             for (VWidget<?> widget : hoveredWidgets) {
                 runnable.accept(widget);
             }
+        }
+    }
+
+    public static void forHoveredWidget(int mouseX, int mouseY, Consumer<VWidget<?>> runnable) {
+        forHoveredWidget(mouseX, mouseY, runnable, null);
+    }
+
+    public static void forHoveredWidgetIfEmpty(int mouseX, int mouseY, Consumer<VeraApp> runnable) {
+        for (VeraApp app : MCVeraData.visibleApplications) {
+            List<VWidget<?>> hoveredWidgets = app.getHoveredWidgets(mouseX, mouseY);
+            if (hoveredWidgets.isEmpty()) runnable.accept(app);
         }
     }
 }
