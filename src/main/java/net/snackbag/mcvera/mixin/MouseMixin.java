6428c8d0354266968153459896f84d1d8c68c803
JXSnack <jx.snack@gmail.com>
2024-12-22 11:35:05 +0100

    set cursor

---
+++ src/main/java/net/snackbag/mcvera/mixin/MouseMixin.java
@@ -0,0 +1,33 @@
+package net.snackbag.mcvera.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.Mouse;
+import net.snackbag.mcvera.MCVeraData;
+import net.snackbag.vera.Vera;
+import net.snackbag.vera.core.VCursorShape;
+import net.snackbag.vera.core.VeraApp;
+import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Mouse.class)
+public abstract class MouseMixin {
+    @Shadow @Final private MinecraftClient client;
+
+    @Inject(method = "onCursorPos", at = @At("HEAD"))
+    private void mcvera$onCursorMove(long window, double x, double y, CallbackInfo ci) {
+        if (client.getWindow().getHandle() != window) return;
+
+        VeraApp selectedApp = null;
+        for (VeraApp app : MCVeraData.visibleApplications) {
+            if (app.isMouseOverApp((int) x, (int) y)) selectedApp = app;
+        }
+
+        if (selectedApp == null) return;
+        GLFW.glfwSetCursor(window, selectedApp.getCursorShape().getGLFWCursor());
+    }
+}
