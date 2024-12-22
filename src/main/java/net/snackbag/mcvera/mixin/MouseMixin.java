988b3d720af9023219d9a3c916ce022836c42d59
JXSnack <jx.snack@gmail.com>
2024-12-22 22:30:03 +0100

    fire mouse move

--- src/main/java/net/snackbag/mcvera/mixin/MouseMixin.java
+++ src/main/java/net/snackbag/mcvera/mixin/MouseMixin.java
@@ -2,11 +2,7 @@ package net.snackbag.mcvera.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
-import net.snackbag.mcvera.MCVeraData;
 import net.snackbag.vera.Vera;
-import net.snackbag.vera.core.VCursorShape;
-import net.snackbag.vera.core.VeraApp;
-import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,9 +15,16 @@ public abstract class MouseMixin {
     @Shadow @Final private MinecraftClient client;
 
     @Inject(method = "onCursorPos", at = @At("HEAD"))
-    private void mcvera$onCursorMove(long window, double x, double y, CallbackInfo ci) {
+    private void mcvera$onCursorMove(long window, double fx, double fy, CallbackInfo ci) {
         if (client.getWindow().getHandle() != window) return;
 
+        double scaleFactor = client.getWindow().getScaleFactor();
 
+        int scaledX = (int) (fx / scaleFactor);
+        int scaledY = (int) (fy / scaleFactor);
+
+        Vera.forHoveredWidget(scaledX, scaledY, (widget) -> {
+            widget.fireEvent("mouse-move", scaledX, scaledY);
+        });
     }
 }
