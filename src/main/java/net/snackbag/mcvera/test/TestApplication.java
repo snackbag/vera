f377929f6f9aa1dfc82900e47fde43741019405c
JXSnack <jx.snack@gmail.com>
2024-12-26 21:42:46 +0100

    larger numbers to test

--- src/main/java/net/snackbag/mcvera/test/TestApplication.java
+++ src/main/java/net/snackbag/mcvera/test/TestApplication.java
@@ -78,7 +78,7 @@ public class TestApplication extends VeraApp {
         dropdown.addItem("roolio", Identifier.of(Identifier.DEFAULT_NAMESPACE, "textures/block/dirt.png"));
         dropdown.addItem("buger", () -> System.out.println("pressed"));
         dropdown.move(90);
-        dropdown.setItemSpacing(3);
+        dropdown.setItemSpacing(16);
         dropdown.modifyHoverFont().color(VColor.white());
         dropdown.setItemHoverColor(VColor.black());
         dropdown.onFocusStateChange(() -> System.out.println("focus state change: " + dropdown.isFocused()));
