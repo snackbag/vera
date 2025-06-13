5ec62eed637590d13aa862ecf926fce7fe42f378
JXSnack <jx.snack@gmail.com>
2025-06-13 20:43:27 +0200

    rename methods to something shorter

--- src/main/java/net/snackbag/vera/widget/VWidget.java
+++ src/main/java/net/snackbag/vera/widget/VWidget.java
@@ -165,11 +165,11 @@ public abstract class VWidget<T extends VWidget<T>> {
         return style != null ? style : dflt;
     }
 
-    public VColor.ColorModifier modifyKeyAsColor(String key) {
+    public VColor.ColorModifier modifyColor(String key) {
         return app.styleSheet.modifyKeyAsColor(this, key);
     }
 
-    public VFont.FontModifier modifyKeyAsFont(String key) {
+    public VFont.FontModifier modifyFont(String key) {
         return app.styleSheet.modifyKeyAsFont(this, key);
     }
 
