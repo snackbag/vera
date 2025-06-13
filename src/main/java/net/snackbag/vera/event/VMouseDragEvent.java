2afc74ab81960455861c2024c48934d258097e7d
JXSnack <jx.snack@gmail.com>
2025-06-13 22:28:33 +0200

    add direction

--- src/main/java/net/snackbag/vera/event/VMouseDragEvent.java
+++ src/main/java/net/snackbag/vera/event/VMouseDragEvent.java
@@ -3,6 +3,13 @@ package net.snackbag.vera.event;
 public interface VMouseDragEvent {
     void run(Context ctx);
 
-    record Context(int startX, int startY, int currentX, int currentY, int moveX, int moveY) {
+    record Context(int startX, int startY, int currentX, int currentY, int moveX, int moveY, Direction direction) {
+    }
+
+    enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
     }
 }
