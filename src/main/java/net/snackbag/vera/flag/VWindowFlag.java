befd8adcb5797c9bfa2534efbe4f7acafa47ff04
JXSnack <jx.snack@gmail.com>
2025-06-13 23:19:56 +0200

    add flags

---
+++ src/main/java/net/snackbag/vera/flag/VWindowFlag.java
@@ -0,0 +1,5 @@
+package net.snackbag.vera.flag;
+
+public enum VWindowFlag {
+    DEBUG
+}
---
+++ src/main/java/net/snackbag/vera/flag/VWindowPositioningFlag.java
@@ -0,0 +1,38 @@
+package net.snackbag.vera.flag;
+
+public enum VWindowPositioningFlag {
+    /**
+     * Renders under the HUD
+     */
+    UNDER_HUD,
+
+    /**
+     * Renders on the same level as the HUD
+     */
+    HUD,
+
+    /**
+     * Renders over the HUD and under normal GUI
+     */
+    HUD_OVERLAY,
+
+    /**
+     * Renders on the same layer as the GUI
+     */
+    GUI,
+
+    /**
+     * Renders over the GUI but under any other render events
+     */
+    GUI_OVERLAY,
+
+    /**
+     * Renders over all other render events
+     */
+    SCREEN,
+
+    /**
+     * Should only be used when wanting to 100% override something
+     */
+    TOP
+}
