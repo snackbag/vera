811a9ceecb4945e0944c38f4c1de20b8c62566ec
JXSnack <jx.snack@gmail.com>
2025-06-17 00:30:28 +0200

    asTopHierarchy method

--- src/main/java/net/snackbag/mcvera/MCVeraData.java
+++ src/main/java/net/snackbag/mcvera/MCVeraData.java
@@ -2,8 +2,10 @@ package net.snackbag.mcvera;
 
 import net.snackbag.vera.core.VeraApp;
 import net.snackbag.vera.flag.VWindowPositioningFlag;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
+import java.util.function.Consumer;
 
 public class MCVeraData {
     public static LinkedHashSet<VeraApp> applications = new LinkedHashSet<>();
@@ -15,4 +17,17 @@ public class MCVeraData {
 
     public static final List<Integer> pressedKeys = new ArrayList<>();
     public static List<Integer> previousPressedKeys = new ArrayList<>();
+
+    /**
+     * Executes a method as the top hierarchy app. If there is no top app it won't execute the specified code.
+     *
+     * @param runnable code to execute
+     * @return whether something has been executed
+     */
+    public static boolean asTopHierarchy(@NotNull Consumer<VeraApp> runnable) {
+        if (appHierarchy.isEmpty()) return false;
+
+        runnable.accept(appHierarchy.get(0));
+        return true;
+    }
 }
