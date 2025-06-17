13b06787bb49bed3690e4baabca4abedddf0d7fe
JXSnack <jx.snack@gmail.com>
2025-06-17 16:53:05 +0200

    turn path into identifier

--- src/main/java/net/snackbag/vera/widget/VImage.java
+++ src/main/java/net/snackbag/vera/widget/VImage.java
@@ -5,30 +5,16 @@ import net.snackbag.vera.Vera;
 import net.snackbag.vera.core.VeraApp;
 
 public class VImage extends VWidget<VImage> {
-    private Identifier path;
-
     public VImage(Identifier path, int width, int height, VeraApp app) {
         super(0, 0, width, height, app);
 
-        this.path = path;
+        setStyle("src", path);
         this.focusOnClick = false;
-    }
-
-    public Identifier getPath() {
-        return path;
-    }
-
-    public void setPath(Identifier path) {
-        this.path = path;
-    }
-
-    public void setPath(String path) {
-        setPath(new Identifier(path));
     }
 
     @Override
     public void render() {
         VeraApp app = getApp();
-        Vera.renderer.drawImage(app, x, y, width, height, rotation, path);
+        Vera.renderer.drawImage(app, x, y, width, height, rotation, getStyle("path"));
     }
 }
