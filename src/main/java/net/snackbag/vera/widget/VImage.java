9b6e1f8bf1877aa25053a42e50b7a20799c13167
JXSnack <jx.snack@gmail.com>
2025-06-18 22:53:45 +0200

    path -> src (in argument names)

--- src/main/java/net/snackbag/vera/widget/VImage.java
+++ src/main/java/net/snackbag/vera/widget/VImage.java
@@ -6,15 +6,15 @@ import net.snackbag.vera.core.VeraApp;
 import net.snackbag.vera.style.StyleState;
 
 public class VImage extends VWidget<VImage> {
-    public VImage(Identifier path, int width, int height, VeraApp app) {
+    public VImage(Identifier src, int width, int height, VeraApp app) {
         super(0, 0, width, height, app);
 
-        setStyle("src", path);
+        setStyle("src", src);
         this.focusOnClick = false;
     }
 
-    public VImage(String path, int width, int height, VeraApp app) {
-        this(new Identifier(path), width, height, app);
+    public VImage(String src, int width, int height, VeraApp app) {
+        this(new Identifier(src), width, height, app);
     }
 
     @Override
