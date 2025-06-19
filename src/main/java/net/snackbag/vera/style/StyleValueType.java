a7398bdd401dff7d52a86078e815e689cc035e5c
JXSnack <jx.snack@gmail.com>
2025-06-19 10:36:48 +0200

    add V4Int and V4Color support

--- src/main/java/net/snackbag/vera/style/StyleValueType.java
+++ src/main/java/net/snackbag/vera/style/StyleValueType.java
@@ -2,9 +2,7 @@ package net.snackbag.vera.style;
 
 import net.minecraft.util.Identifier;
 import net.snackbag.mcvera.MinecraftVera;
-import net.snackbag.vera.core.VColor;
-import net.snackbag.vera.core.VCursorShape;
-import net.snackbag.vera.core.VFont;
+import net.snackbag.vera.core.*;
 import org.apache.commons.lang3.EnumUtils;
 import org.jetbrains.annotations.Nullable;
 
@@ -16,7 +14,10 @@ public enum StyleValueType {
 
     COLOR(VColor.black()),
     FONT(VFont.create()),
-    CURSOR(VCursorShape.DEFAULT);
+    CURSOR(VCursorShape.DEFAULT),
+
+    V4INT(new V4Int(0)),
+    V4COLOR(new V4Color(VColor.black()));
 
     public final Object standard;
 
@@ -31,6 +32,8 @@ public enum StyleValueType {
             return STRING;
         }
 
+        else if (val instanceof V4Color || val instanceof VColor[]) return V4COLOR;
+        else if (val instanceof V4Int || val instanceof int[]) return V4INT;
         else if (val instanceof Identifier) return IDENTIFIER;
         else if (val instanceof VCursorShape) return CURSOR;
         else if (val instanceof Integer) return INT;
@@ -44,6 +47,24 @@ public enum StyleValueType {
         if (value instanceof String v) {
             if (to == IDENTIFIER) return new Identifier(v);
             else if (to == CURSOR) return EnumUtils.getEnumIgnoreCase(VCursorShape.class, v);
+        }
+
+        else if (value instanceof int[] v) {
+            return switch (v.length) {
+                case 1 -> new V4Int(v[0]);
+                case 2 -> new V4Int(v[0], v[1]);
+                case 4 -> new V4Int(v[0], v[1], v[2], v[3]);
+                default -> throw new RuntimeException("invalid V4Int format. Length must be 1, 2 or 4. Provided: %d".formatted(v.length));
+            };
+        }
+
+        else if (value instanceof VColor[] v) {
+            return switch (v.length) {
+                case 1 -> new V4Color(v[0]);
+                case 2 -> new V4Color(v[0], v[1]);
+                case 4 -> new V4Color(v[0], v[1], v[2], v[3]);
+                default -> throw new RuntimeException("invalid V4Color format. Length must be 1, 2 or 4. Provided: %d".formatted(v.length));
+            };
         }
 
         else if (to == FLOAT && value instanceof Double v) return v.floatValue();
