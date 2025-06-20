4faf81ec2b03886a0c14afaf19f10acc78ab92c2
JXSnack <jx.snack@gmail.com>
2025-06-20 22:55:02 +0200

    test rect

--- src/main/java/net/snackbag/mcvera/test/StyleTestApplication.java
+++ src/main/java/net/snackbag/mcvera/test/StyleTestApplication.java
@@ -1,10 +1,12 @@
 package net.snackbag.mcvera.test;
 
-import net.minecraft.util.Identifier;
-import net.snackbag.mcvera.MinecraftVera;
+import net.snackbag.vera.core.VColor;
+import net.snackbag.vera.core.VCursorShape;
 import net.snackbag.vera.core.VeraApp;
 import net.snackbag.vera.event.VShortcut;
+import net.snackbag.vera.style.StyleState;
 import net.snackbag.vera.widget.VLabel;
+import net.snackbag.vera.widget.VRect;
 
 public class StyleTestApplication extends VeraApp {
     public static final StyleTestApplication INSTANCE = new StyleTestApplication();
@@ -16,5 +18,13 @@ public class StyleTestApplication extends VeraApp {
         //loadStyleSheet(new Identifier(MinecraftVera.MOD_ID, "demo/test.vss"));
 
         new VLabel("helo", this).alsoAdd();
+
+        VRect testRect = new VRect(VColor.black(), this).alsoAdd();
+        testRect.setStyle("color", StyleState.HOVERED, VColor.white());
+
+        testRect.setStyle("cursor", StyleState.HOVERED, VCursorShape.POINTING_HAND);
+        testRect.setStyle("cursor", StyleState.CLICKED, VCursorShape.ALL_RESIZE);
+
+        testRect.onMouseDragLeft((ctx) -> testRect.move(testRect.getX() + ctx.moveX(), testRect.getY() + ctx.moveY()));
     }
 }
