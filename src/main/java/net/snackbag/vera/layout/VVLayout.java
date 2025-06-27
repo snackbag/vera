a2e53a29b6d3b5f8a0e2341189779a57f664f217
JXSnack <jx.snack@gmail.com>
2025-06-27 18:08:57 +0200

    implement vertical layout

--- src/main/java/net/snackbag/vera/layout/VVLayout.java
+++ src/main/java/net/snackbag/vera/layout/VVLayout.java
@@ -1,6 +1,8 @@
 package net.snackbag.vera.layout;
 
+import net.snackbag.vera.VElement;
 import net.snackbag.vera.core.VeraApp;
+import org.joml.Vector2i;
 
 public class VVLayout extends VLayout {
     public VVLayout(VeraApp app, int x, int y, int width, int height) {
@@ -9,5 +11,16 @@ public class VVLayout extends VLayout {
 
     public VVLayout(VeraApp app, int x, int y) {
         this(app, x, y, -1, -1);
+    }
+
+    @Override
+    public void rebuild() {
+        int y = 0;
+
+        for (VElement elem : elements) {
+            cache.put(elem, new Vector2i(x, y));
+
+            y += elem.getHeight();
+        }
     }
 }
