c1cbb2ec819d566d9115bdd8d72cecbe023ee546
JXSnack <jx.snack@gmail.com>
2025-06-27 17:14:23 +0200

    update to super

--- src/main/java/net/snackbag/vera/layout/VHLayout.java
+++ src/main/java/net/snackbag/vera/layout/VHLayout.java
@@ -1,4 +1,13 @@
 package net.snackbag.vera.layout;
 
+import net.snackbag.vera.core.VeraApp;
+
 public class VHLayout extends VLayout {
+    public VHLayout(VeraApp app, int x, int y, int width, int height) {
+        super(app, x, y, width, height);
+    }
+
+    public VHLayout(VeraApp app, int x, int y) {
+        this(app, x, y, -1, -1);
+    }
 }
--- src/main/java/net/snackbag/vera/layout/VVLayout.java
+++ src/main/java/net/snackbag/vera/layout/VVLayout.java
@@ -1,4 +1,13 @@
 package net.snackbag.vera.layout;
 
+import net.snackbag.vera.core.VeraApp;
+
 public class VVLayout extends VLayout {
+    public VVLayout(VeraApp app, int x, int y, int width, int height) {
+        super(app, x, y, width, height);
+    }
+
+    public VVLayout(VeraApp app, int x, int y) {
+        this(app, x, y, -1, -1);
+    }
 }
