ad7ddcbc6a3bf9af72ba3c237a0771d00cb1f3a8
JXSnack <jx.snack@gmail.com>
2025-06-27 19:50:49 +0200

    clean up layouts

--- src/main/java/net/snackbag/mcvera/test/LayoutTestApplication.java
+++ src/main/java/net/snackbag/mcvera/test/LayoutTestApplication.java
@@ -18,11 +18,11 @@ public class LayoutTestApplication extends VeraApp {
         new VLabel("I'm a test", this).alsoAddTo(layout);
         new VLabel("I'm another test", this).alsoAddTo(layout);
 
-        VLayout secondLayout = new VHLayout(this, 0, 0).alsoAddTo(layout);
+        VLayout secondLayout = new VHLayout(layout);
         new VLabel("1", this).alsoAddTo(secondLayout);
         new VLabel("2", this).alsoAddTo(secondLayout);
 
-        VLayout thirdLayout = new VVLayout(this, 0, 0).alsoAddTo(secondLayout);
+        VLayout thirdLayout = new VVLayout(secondLayout);
         new VLabel("oh?", this).alsoAddTo(thirdLayout);
         new VLabel("oh!!!!", this).alsoAddTo(thirdLayout);
     }
