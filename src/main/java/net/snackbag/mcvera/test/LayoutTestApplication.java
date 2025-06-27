3ef4945e915f711ab5cc6244e253b732e9221b13
JXSnack <jx.snack@gmail.com>
2025-06-27 18:59:07 +0200

    very cursed testing setup

--- src/main/java/net/snackbag/mcvera/test/LayoutTestApplication.java
+++ src/main/java/net/snackbag/mcvera/test/LayoutTestApplication.java
@@ -2,6 +2,9 @@ package net.snackbag.mcvera.test;
 
 import net.snackbag.vera.core.VeraApp;
 import net.snackbag.vera.event.VShortcut;
+import net.snackbag.vera.layout.VHLayout;
+import net.snackbag.vera.layout.VLayout;
+import net.snackbag.vera.layout.VVLayout;
 import net.snackbag.vera.widget.VLabel;
 
 public class LayoutTestApplication extends VeraApp {
@@ -11,13 +14,19 @@ public class LayoutTestApplication extends VeraApp {
     public void init() {
         new VShortcut(this, "escape", this::hide).alsoAdd();
 
-        /*
-        VLayout layout = new VVLayout(this);
-        VLabel label = new VLabel("I'm a test", this).alsoAddTo(layout);
+        VLayout layout = new VVLayout(this, 0, 0);
+        VLabel label = new VLabel("I'm a test", this).alsoAdd(); layout.addElement(label);
+        VLabel label2 = new VLabel("I'm another test", this).alsoAdd(); layout.addElement(label2);
 
-        VLayout secondLayout = new VHLayout(this);
-        VLabel horiz1 = new VLabel("1", this).alsoAddTo(secondLayout);
-        VLabel horiz2 = new VLabel("2", this).alsoAddTo(secondLayout);
-         */
+        VLayout secondLayout = new VHLayout(this, 0, 0);
+        VLabel horiz1 = new VLabel("1", this).alsoAdd(); secondLayout.addElement(horiz1);
+        VLabel horiz2 = new VLabel("2", this).alsoAdd(); secondLayout.addElement(horiz2);
+
+        VLayout thirdLayout = new VVLayout(this, 0, 0);
+        VLabel label3 = new VLabel("oh?", this).alsoAdd(); thirdLayout.addElement(label3);
+        VLabel label4 = new VLabel("oh!!!!", this).alsoAdd(); thirdLayout.addElement(label4);
+
+        secondLayout.addElement(thirdLayout);
+        layout.addElement(secondLayout);
     }
 }
