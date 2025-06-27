f2d2c8e28ca25971a999e10b0ba8b3078211ca8b
JXSnack <jx.snack@gmail.com>
2025-06-27 21:09:53 +0200

    add fromInt method

--- src/main/java/net/snackbag/vera/core/VMouseButton.java
+++ src/main/java/net/snackbag/vera/core/VMouseButton.java
@@ -3,5 +3,14 @@ package net.snackbag.vera.core;
 public enum VMouseButton {
     LEFT,
     MIDDLE,
-    RIGHT
+    RIGHT;
+
+    public static VMouseButton fromInt(int button) {
+        return switch (button) {
+            case 0 -> LEFT;
+            case 1 -> RIGHT;
+            case 2 -> MIDDLE;
+            default -> throw new IllegalArgumentException("Invalid button type: %d".formatted(button));
+        };
+    }
 }
