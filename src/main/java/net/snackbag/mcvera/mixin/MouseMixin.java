f70268c3266a7b524ef439d295ebf1037465d27f
JXSnack <jx.snack@gmail.com>
2025-06-27 21:09:22 +0200

    call move for DragHandler

--- src/main/java/net/snackbag/mcvera/mixin/MouseMixin.java
+++ src/main/java/net/snackbag/mcvera/mixin/MouseMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.client.Mouse;
 import net.snackbag.vera.Vera;
 import net.snackbag.vera.core.VCursorShape;
 import net.snackbag.vera.core.VeraApp;
+import net.snackbag.vera.util.DragHandler;
 import net.snackbag.vera.widget.VWidget;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -37,6 +38,8 @@ public abstract class MouseMixin {
             if (widget != null) widget.events.fire("mouse-move", mouseX, mouseY);
             else if (app.getCursorShape() != VCursorShape.DEFAULT) app.setCursorShape(VCursorShape.DEFAULT);
         });
+
+        DragHandler.move();
     }
 
     @Inject(method = "onFilesDropped", at = @At("HEAD"))
