99e1d29a6b4aae10aa31bd563670a909ccb33269
JXSnack <jx.snack@gmail.com>
2025-06-27 21:48:30 +0200

    test for classes and multiple stylesheets

--- src/main/java/net/snackbag/mcvera/test/StyleTestApplication.java
+++ src/main/java/net/snackbag/mcvera/test/StyleTestApplication.java
@@ -2,9 +2,11 @@ package net.snackbag.mcvera.test;
 
 import net.snackbag.vera.core.VColor;
 import net.snackbag.vera.core.VCursorShape;
+import net.snackbag.vera.core.VFont;
 import net.snackbag.vera.core.VeraApp;
 import net.snackbag.vera.event.VShortcut;
 import net.snackbag.vera.style.StyleState;
+import net.snackbag.vera.style.VStyleSheet;
 import net.snackbag.vera.widget.VLabel;
 import net.snackbag.vera.widget.VRect;
 
@@ -15,9 +17,11 @@ public class StyleTestApplication extends VeraApp {
     public void init() {
         new VShortcut(this, "escape", this::hide).alsoAdd();
 
-        //loadStyleSheet(new Identifier(MinecraftVera.MOD_ID, "demo/test.vss"));
+        mergeStyleSheet(createStyleSheet());
 
-        new VLabel("helo", this).alsoAdd();
+        new VLabel("helo", this)
+                .alsoAddClass("label")
+                .alsoAdd();
 
         VRect testRect = new VRect(VColor.black(), this).alsoAdd();
         testRect.setStyle("color", StyleState.HOVERED, VColor.white());
@@ -26,5 +30,14 @@ public class StyleTestApplication extends VeraApp {
         testRect.setStyle("cursor", StyleState.CLICKED, VCursorShape.ALL_RESIZE);
 
         testRect.onMouseDragLeft((ctx) -> testRect.move(testRect.getX() + ctx.moveX(), testRect.getY() + ctx.moveY()));
+    }
+
+    public VStyleSheet createStyleSheet() {
+        VStyleSheet sheet = new VStyleSheet();
+
+        sheet.setKey("label", "font", VFont.create());
+        sheet.setKey("label", "font", VFont.create().withColor(VColor.of(20)), StyleState.HOVERED);
+
+        return sheet;
     }
 }
