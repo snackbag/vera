416dda8e8ba4f052f0733fd9e33379f4be8f9bbd
JXSnack <jx.snack@gmail.com>
2025-07-16 16:15:51 +0200

    standard style

---
+++ src/main/java/net/snackbag/vera/style/standard/VStandardStyle.java
@@ -0,0 +1,8 @@
+package net.snackbag.vera.style.standard;
+
+import net.snackbag.vera.style.VStyleSheet;
+
+@FunctionalInterface
+public interface VStandardStyle {
+    void apply(VStyleSheet sheet);
+}
