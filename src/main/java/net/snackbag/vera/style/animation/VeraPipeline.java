09709fd8dc339932fc5b5a10e2e3c6ed4c19bc0a
JXSnack <jx.snack@gmail.com>
2025-07-18 13:53:22 +0200

    implement composite uniform generator

---
+++ src/main/java/net/snackbag/vera/style/animation/VeraPipeline.java
@@ -0,0 +1,46 @@
+package net.snackbag.vera.style.animation;
+
+import net.snackbag.vera.Vera;
+import net.snackbag.vera.core.VeraApp;
+import net.snackbag.vera.style.StyleValueType;
+import net.snackbag.vera.style.animation.composite.Composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VeraPipeline {
+    public final VeraApp app;
+    private final List<Composite> passes = new ArrayList<>();
+
+    public VeraPipeline(VeraApp app) {
+        this.app = app;
+    }
+
+    public void addPass(Composite pass) {
+        setupPass(pass);
+        this.passes.add(pass);
+    }
+
+    public void addPass(int index, Composite pass) {
+        setupPass(pass);
+        this.passes.add(index, pass);
+    }
+
+    private void setupPass(Composite pass) {
+        boolean unique = pass.pipeline.setSafe(this);
+        if (!unique) throw new UnsupportedOperationException("Cannot setup composite pass if composite is already bound to pipeline");
+    }
+
+    public <T> T applyComposites(AnimationEngine engine, String style, StyleValueType type, T in) {
+        for (Composite pass : passes) {
+            if (pass.frameTime != Vera.renderCacheId) {
+                pass.frameTime = Vera.renderCacheId;
+                pass.generateUniforms();
+            }
+
+            in = pass.apply(engine, style, type, in);
+        }
+
+        return in;
+    }
+}
