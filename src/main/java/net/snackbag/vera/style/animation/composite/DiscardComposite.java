f43bd7b815d588d1f0c177449f9f1e06030c5c00
JXSnack <jx.snack@gmail.com>
2025-07-18 19:43:49 +0200

    update composites to fit new design

--- src/main/java/net/snackbag/vera/style/animation/composite/DiscardComposite.java
+++ src/main/java/net/snackbag/vera/style/animation/composite/DiscardComposite.java
@@ -1,11 +1,8 @@
 package net.snackbag.vera.style.animation.composite;
 
-import net.snackbag.vera.style.StyleValueType;
-import net.snackbag.vera.style.animation.AnimationEngine;
-
 public class DiscardComposite extends Composite {
     @Override
-    public <T> T apply(AnimationEngine engine, String style, StyleValueType type, T in) {
-        return in;
+    public <T> T apply(Context<T> ctx) {
+        return ctx.in();
     }
 }
