6c89559b05c72d3539f76b843fcb635e48cbba6a
JXSnack <jx.snack@gmail.com>
2025-07-21 17:40:44 +0200

    cache animation times

--- src/main/java/net/snackbag/vera/style/animation/composite/AnimationComposite.java
+++ src/main/java/net/snackbag/vera/style/animation/composite/AnimationComposite.java
@@ -8,27 +8,40 @@ import java.util.HashMap;
 
 public class AnimationComposite extends Composite {
     private final HashMap<AnimationEngine, VAnimation[]> animations = new HashMap<>();
+    private final HashMap<VAnimation, Long> animationTimes = new HashMap<>();
 
     @Override
     public void generateUniforms() {
         animations.clear();
+        animationTimes.clear();
     }
 
     @Override
     public void applyWidget(VWidget<?> widget) {
-        animations.put(widget.animations, widget.animations.getAllActive());
+        VAnimation[] active = widget.animations.getAllActive();
+        animations.put(widget.animations, active);
+
+        for (VAnimation animation : active) {
+            animationTimes.put(animation, System.currentTimeMillis() - widget.animations.getTimeSinceActive(animation));
+        }
     }
 
     @Override
     public <T> T applyStyle(Context<T> ctx, T in, boolean isNewFrame) {
-        // No caching necessary, since this is done earlier.
+        // Most caching isn't necessary, since it's done earlier.
 
         AnimationEngine engine = ctx.engine();
 
         T out = in;
         for (VAnimation animation : animations.get(engine)) {
             // No need to check affects, since calculateStyle does this internally
-            T rv = animation.calculateStyle(ctx.style(), ctx.original(), ctx.type(), System.currentTimeMillis() - engine.getTimeSinceActive(animation));
+            T rv = animation.calculateStyle(
+                    ctx.style(),
+                    ctx.original(),
+                    ctx.type(),
+                    animationTimes.get(animation)
+            );
+
             if (rv != null) out = rv;
         }
 
