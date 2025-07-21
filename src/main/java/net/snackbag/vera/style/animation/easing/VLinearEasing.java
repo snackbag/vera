3b3e1cc2316a4022253b51683996605cfd76dfa2
JXSnack <jx.snack@gmail.com>
2025-07-21 15:57:55 +0200

    add apply for integers

--- src/main/java/net/snackbag/vera/style/animation/easing/VLinearEasing.java
+++ src/main/java/net/snackbag/vera/style/animation/easing/VLinearEasing.java
@@ -7,4 +7,10 @@ public class VLinearEasing extends VEasing {
     public float apply(float from, float to, float delta) {
         return from + delta * (to - from);
     }
+
+    @Override
+    public int apply(int from, int to, float delta) {
+        final float fromF = (float) from;
+        return Math.round(fromF + delta * ((float) to - fromF));
+    }
 }
