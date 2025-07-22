bf39df87540d879f7e1ab5f696ba382cc4da25b0
JXSnack <jx.snack@gmail.com>
2025-07-22 10:57:55 +0200

    very basic and unoptimized UnwindComposite

--- src/main/java/net/snackbag/vera/style/animation/composite/UnwindComposite.java
+++ src/main/java/net/snackbag/vera/style/animation/composite/UnwindComposite.java
@@ -1,4 +1,36 @@
 package net.snackbag.vera.style.animation.composite;
 
+import net.snackbag.vera.style.animation.AnimationEngine;
+import net.snackbag.vera.style.animation.VAnimation;
+
 public class UnwindComposite extends Composite {
+    private long time;
+
+    @Override
+    public void generateUniforms() {
+        time = System.currentTimeMillis();
+    }
+
+    @Override
+    public <T> T applyStyle(Context<T> ctx, T in, boolean isNewFrame) {
+        AnimationEngine engine = ctx.engine();
+
+        T out = in;
+
+        for (VAnimation animation : engine.getAllUnwinding()) {
+            if (!animation.affects(ctx.style())) continue;
+            if (animation.unwindTime <= 0) {
+                out = ctx.original();
+                continue;
+            }
+
+            long sinceUnwinding = engine.getTimeSinceUnwinding(animation);
+            int relativeTime = (int) (time - sinceUnwinding);
+            float delta = Math.min((float) relativeTime / (float) animation.unwindTime, 1f);
+
+            out = (T) ctx.type().animationTransition.apply(in, ctx.original(), animation.unwindEasing, delta);
+        }
+
+        return out;
+    }
 }
