a6bff5418d46e352f8422192b9cc3d5afe95c09e
JXSnack <jx.snack@gmail.com>
2025-08-25 22:39:06 +0200

    fix center alignment halving original position

--- src/main/java/net/snackbag/vera/layout/VVLayout.java
+++ src/main/java/net/snackbag/vera/layout/VVLayout.java
@@ -37,7 +37,7 @@ public class VVLayout extends VLayout {
     protected Vector2i applyAlignment(Vector2i original) {
         return switch (alignment) {
             case START -> original;
-            case CENTER -> new Vector2i(original.x, getHeight() / 2 - calculateElementsHeight() / 2 + original.y / 2);
+            case CENTER -> new Vector2i(original.x, getHeight() / 2 - calculateElementsHeight() / 2 + original.y);
             case END -> new Vector2i(original.x, getHeight() - calculateElementsHeight() + original.y);
         };
     }
