2c7aa71e29b6fe845dacc350d47329ac7cd037d7
JXSnack <jx.snack@gmail.com>
2025-10-25 16:54:36 +0200

    register all easings in the registrar

--- src/main/java/net/snackbag/mcvera/impl/MCVeraRegistrar.java
+++ src/main/java/net/snackbag/mcvera/impl/MCVeraRegistrar.java
@@ -1,13 +1,17 @@
 package net.snackbag.mcvera.impl;
 
+import net.snackbag.vera.style.animation.easing.VEasing;
 import net.snackbag.vera.style.standard.VStandardStyle;
 import net.snackbag.vera.style.VStyleSheet;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MCVeraRegistrar {
     private final List<VStandardStyle> standardStyles = new ArrayList<>();
+    private final HashMap<String, VEasing> easings = new HashMap<>();
 
     public void registerStandardStyle(VStandardStyle style) {
         standardStyles.add(style);
@@ -18,5 +22,13 @@ public class MCVeraRegistrar {
             standardStyle.reserve(sheet);
             standardStyle.apply(sheet);
         }
+    }
+
+    public void registerEasing(String name, VEasing easing) {
+        easings.put(name.toLowerCase(), easing);
+    }
+
+    public @Nullable VEasing getEasingIgnoreCase(String name) {
+        return easings.getOrDefault(name.toLowerCase(), null);
     }
 }
--- src/main/java/net/snackbag/vera/style/animation/easing/Easings.java
+++ src/main/java/net/snackbag/vera/style/animation/easing/Easings.java
@@ -1,5 +1,11 @@
 package net.snackbag.vera.style.animation.easing;
 
+import net.snackbag.vera.Vera;
+
 public class Easings {
     public static final VLinearEasing LINEAR = new VLinearEasing();
+
+    public static VEasing getIgnoreCase(String name) {
+        return Vera.registrar.getEasingIgnoreCase(name);
+    }
 }
