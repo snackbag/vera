421998e819b7947d0be97bd9e9079a01c70557de
JXSnack <jx.snack@gmail.com>
2025-10-25 16:54:49 +0200

    add easing SVT

--- src/main/java/net/snackbag/vera/style/StyleValueType.java
+++ src/main/java/net/snackbag/vera/style/StyleValueType.java
@@ -5,6 +5,7 @@ import net.snackbag.mcvera.MinecraftVera;
 import net.snackbag.vera.core.*;
 import net.snackbag.vera.core.v4.V4Color;
 import net.snackbag.vera.core.v4.V4Int;
+import net.snackbag.vera.style.animation.easing.Easings;
 import net.snackbag.vera.style.animation.easing.VEasing;
 import org.apache.commons.lang3.EnumUtils;
 import org.jetbrains.annotations.Nullable;
@@ -21,6 +22,7 @@ public enum StyleValueType {
                     .withSize(easing.apply(from.getSize(), to.getSize(), delta))
                     .withName(delta > 0.5 ? to.getName() : from.getName())),
     CURSOR(VCursorShape.DEFAULT, (f, t, e, d) -> d > 0.5 ? t : f),
+    EASING(Easings.LINEAR, (f, t, e, d) -> d > 0.5 ? t : f),
 
     V4INT(new V4Int(0), (from, to, easing, delta) -> new V4Int(
             easing.apply(from.get1(), to.get1(), delta),
@@ -47,6 +49,7 @@ public enum StyleValueType {
         if (val instanceof String s) {
             if (bias == IDENTIFIER && s.matches("^[\\w-./]*:[\\w-./]*$")) return IDENTIFIER;
             else if (bias == CURSOR && EnumUtils.getEnumIgnoreCase(VCursorShape.class, s) != null) return CURSOR;
+            else if (bias == EASING && Easings.getIgnoreCase(s) != null) return EASING;
             return STRING;
         } else if (val instanceof V4Color || (bias == V4COLOR && (val instanceof VColor[] || val instanceof VColor)))
             return V4COLOR;
@@ -54,6 +57,7 @@ public enum StyleValueType {
             return V4INT;
         else if (val instanceof Identifier) return IDENTIFIER;
         else if (val instanceof VCursorShape) return CURSOR;
+        else if (val instanceof VEasing) return EASING;
         else if (val instanceof Integer) return INT;
         else if (val instanceof Float || val instanceof Double) return FLOAT;
         else if (val instanceof VColor) return COLOR;
@@ -65,6 +69,7 @@ public enum StyleValueType {
         if (value instanceof String v) {
             if (to == IDENTIFIER) return new Identifier(v);
             else if (to == CURSOR) return EnumUtils.getEnumIgnoreCase(VCursorShape.class, v);
+            else if (to == EASING) return Easings.getIgnoreCase(v);
         }
 
         else if (value instanceof int[] || value instanceof Integer[]) {
