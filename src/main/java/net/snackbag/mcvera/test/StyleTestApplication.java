88a62b05a164b0d589c65d36c6ec3bcbf9189751
JXSnack <jx.snack@gmail.com>
2026-02-02 13:53:27 +0100

    final before total rewrite of animation system

--- src/main/java/net/snackbag/mcvera/test/StyleTestApplication.java
+++ src/main/java/net/snackbag/mcvera/test/StyleTestApplication.java
@@ -44,12 +44,11 @@ public class StyleTestApplication extends VeraApp {
 
         VRect testRect = new VRect(VColor.black(), this).alsoAdd();
 //        testRect.onHover(() -> testRect.animations.activateOrRewind(hoverAnimation));
-//        testRect.onHoverLeave(() -> testRect.animations.activate(hoverAnimation));
+//        testRect.onHoverLeave(() -> testRect.animations.unwind(hoverAnimation));
 
         testRect.setStyle("cursor", StyleState.HOVERED, VCursorShape.POINTING_HAND);
         testRect.setStyle("cursor", StyleState.CLICKED, VCursorShape.ALL_RESIZE);
         testRect.setStyle("background-color", StyleState.HOVERED, VColor.white());
-        testRect.setStyle("transition", StyleState.HOVERED, 1000);
 
         testRect.onMouseDragLeft((ctx) -> testRect.move(testRect.getX() + ctx.moveX(), testRect.getY() + ctx.moveY()));
 
