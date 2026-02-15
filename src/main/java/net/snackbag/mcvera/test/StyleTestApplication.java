d95f798f99f6a6a8bcebeb443bee50b9bf3f1259
JXSnack <jx.snack@gmail.com>
2026-02-15 14:59:54 +0100

    new basic animation test

--- src/main/java/net/snackbag/mcvera/test/StyleTestApplication.java
+++ src/main/java/net/snackbag/mcvera/test/StyleTestApplication.java
@@ -1,35 +1,22 @@
 package net.snackbag.mcvera.test;
 
 import net.snackbag.vera.core.VColor;
-import net.snackbag.vera.core.VCursorShape;
 import net.snackbag.vera.core.VFont;
 import net.snackbag.vera.core.VeraApp;
 import net.snackbag.vera.event.VShortcut;
 import net.snackbag.vera.style.StyleState;
 import net.snackbag.vera.style.VStyleSheet;
-import net.snackbag.vera.style.animation.LoopMode;
 import net.snackbag.vera.style.animation.VAnimation;
-import net.snackbag.vera.style.animation.easing.Easings;
 import net.snackbag.vera.widget.VLabel;
 import net.snackbag.vera.widget.VRect;
 
 public class StyleTestApplication extends VeraApp {
     public static StyleTestApplication INSTANCE = new StyleTestApplication();
 
-    private final VAnimation testAnimation = new VAnimation.Builder(this, "test")
-            .unwindTime(2000)
-            .unwindOnFinish()
-            .loop(LoopMode.REPEAT)
-
-            .keyframe(1000, frame -> frame.style("background-color", VColor.MC_GOLD), 2000)
-            .keyframe(1000, frame -> frame.style("background-color", VColor.MC_RED), 2000)
-            .build();
-
-    private final VAnimation hoverAnimation = new VAnimation.Builder(this, "hover")
-            .unwindTime(1000)
-            .keepFinalStyle(StyleState.HOVERED)
-
-            .keyframe(1000, frame -> frame.style("background-color", VColor.MC_WHITE), 0)
+    private final VAnimation testAnimation = new VAnimation.Builder("test")
+            .keyframe(1000, 5000, frame -> {
+                frame.style("background-color", VColor.MC_RED);
+            })
             .build();
 
     @Override
@@ -43,19 +30,7 @@ public class StyleTestApplication extends VeraApp {
                 .alsoAdd();
 
         VRect testRect = new VRect(VColor.black(), this).alsoAdd();
-//        testRect.onHover(() -> testRect.animations.activateOrRewind(hoverAnimation));
-//        testRect.onHoverLeave(() -> testRect.animations.unwind(hoverAnimation));
-
-        testRect.setStyle("cursor", StyleState.HOVERED, VCursorShape.POINTING_HAND);
-        testRect.setStyle("cursor", StyleState.CLICKED, VCursorShape.ALL_RESIZE);
-        testRect.setStyle("background-color", StyleState.HOVERED, VColor.white());
-
-        testRect.onMouseDragLeft((ctx) -> testRect.move(testRect.getX() + ctx.moveX(), testRect.getY() + ctx.moveY()));
-
-        new VShortcut(this, "a", () -> testRect.animations.activate(testAnimation));
-        new VShortcut(this, "u", () -> testRect.animations.unwind(testAnimation));
-        new VShortcut(this, "r", () -> testRect.animations.rewind(testAnimation));
-        new VShortcut(this, "k", () -> testRect.animations.kill(testAnimation));
+        testRect.onLeftClick(() -> testRect.animate(testAnimation));
     }
 
     public VStyleSheet createStyleSheet() {
