5e3c2a6bccab3d9f682e5fce636fb6550c6fbf77
JXSnack <jx.snack@gmail.com>
2026-02-22 13:56:56 +0100

    very basic playback context

---
+++ src/main/java/net/snackbag/vera/style/animation/PlaybackContext.java
@@ -0,0 +1,20 @@
+package net.snackbag.vera.style.animation;
+
+// TODO: winding
+public class PlaybackContext {
+    public final CompiledAnimation animation;
+    public final long startTime;
+
+    public PlaybackContext(CompiledAnimation animation, long startTime) {
+        this.animation = animation;
+        this.startTime = startTime;
+    }
+
+    public int getRelativeTime() {
+        return Math.toIntExact(System.currentTimeMillis() - startTime) % animation.duration;
+    }
+
+    public float getProgress() {
+        return (float) getRelativeTime() / animation.duration;
+    }
+}
