764f4fdc5ddbe40bf10fbb6bcd65dec145a72bde
JXSnack <jx.snack@gmail.com>
2026-02-23 16:19:14 +0100

    remove extra easings, something is weird

--- src/main/java/net/snackbag/vera/style/animation/easing/VEasings.java
+++ src/main/java/net/snackbag/vera/style/animation/easing/VEasings.java
@@ -6,10 +6,6 @@ public class VEasings {
     public static final ImmediateEasing IMMEDIATE = new ImmediateEasing();
     public static final LinearEasing LINEAR = new LinearEasing();
 
-    public static final SineEasing.In SINE_IN = new SineEasing.In();
-    public static final SineEasing.Out SINE_OUT = new SineEasing.Out();
-    public static final SineEasing.InOut SINE_IN_OUT = new SineEasing.InOut();
-
     public static VEasing getDefault() {
         return LINEAR;
     }
