3b834c9b6dda50d258c1fb144a4249dc7344e450
JXSnack <jx.snack@gmail.com>
2026-02-25 13:01:17 +0100

    use proper effective methods

--- src/main/java/net/snackbag/vera/layout/VHLayout.java
+++ src/main/java/net/snackbag/vera/layout/VHLayout.java
@@ -37,8 +37,8 @@ public class VHLayout extends VLayout {
     protected Vector2i applyAlignment(Vector2i original) {
         return switch (alignment) {
             case START -> original;
-            case CENTER -> new Vector2i(getWidth() / 2 - calculateElementsWidth() / 2 + original.x, original.y);
-            case END -> new Vector2i(getWidth() - calculateElementsWidth() + original.x, original.y);
+            case CENTER -> new Vector2i(getEffectiveWidth() / 2 - calculateElementsWidth() / 2 + original.x, original.y);
+            case END -> new Vector2i(getEffectiveWidth() - calculateElementsWidth() + original.x, original.y);
         };
     }
 }
--- src/main/java/net/snackbag/vera/layout/VVLayout.java
+++ src/main/java/net/snackbag/vera/layout/VVLayout.java
@@ -37,8 +37,8 @@ public class VVLayout extends VLayout {
     protected Vector2i applyAlignment(Vector2i original) {
         return switch (alignment) {
             case START -> original;
-            case CENTER -> new Vector2i(original.x, getHeight() / 2 - calculateElementsHeight() / 2 + original.y);
-            case END -> new Vector2i(original.x, getHeight() - calculateElementsHeight() + original.y);
+            case CENTER -> new Vector2i(original.x, getEffectiveHeight() / 2 - calculateElementsHeight() / 2 + original.y);
+            case END -> new Vector2i(original.x, getEffectiveHeight() - calculateElementsHeight() + original.y);
         };
     }
 }
