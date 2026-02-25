82815080e85114738280953d7a2caa22f5a61744
JXSnack <jx.snack@gmail.com>
2026-02-25 20:14:54 +0100

    add VFill to prepare for merge of colors and images

---
+++ src/main/java/net/snackbag/vera/core/VFill.java
@@ -0,0 +1,20 @@
+package net.snackbag.vera.core;
+
+import net.snackbag.vera.style.animation.easing.VEasing;
+
+public interface VFill {
+    static VFill empty() {
+        return new VFill() {
+            @Override
+            public void renderQuad(VRenderContext ctx, int x, int y, int width, int height) {}
+
+            @Override
+            public VFill ease(VEasing easing, VFill target, float delta) {
+                return target;
+            }
+        };
+    }
+
+    void renderQuad(VRenderContext ctx, int x, int y, int width, int height);
+    VFill ease(VEasing easing, VFill target, float delta);
+}
