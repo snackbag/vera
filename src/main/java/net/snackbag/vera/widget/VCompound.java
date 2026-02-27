4ff579f56dea2fd8ae05f0e7f8151f28358b557d
JXSnack <jx.snack@gmail.com>
2026-02-27 19:11:26 +0100

    add compound widgets

---
+++ src/main/java/net/snackbag/vera/widget/VCompound.java
@@ -0,0 +1,107 @@
+package net.snackbag.vera.widget;
+
+import net.snackbag.mcvera.MinecraftVera;
+import net.snackbag.vera.Vera;
+import net.snackbag.vera.core.VAppAccess;
+import net.snackbag.vera.core.VRenderContext;
+import net.snackbag.vera.core.VeraApp;
+import net.snackbag.vera.layout.VLayout;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class VCompound<T extends VWidget<T>> extends VWidget<T> implements VAppAccess {
+    protected final VLayout layout;
+    private final List<VWidget<?>> widgets = new ArrayList<>();
+
+    public VCompound(int x, int y, int width, int height, VLayout layout, VAppAccess app) {
+        super(x, y, width, height, app);
+
+        this.layout = layout;
+        this.layout.move(x, y);
+
+        init();
+    }
+
+    public abstract void init();
+
+    //
+    // App Access
+    //
+
+    @Override
+    public @NotNull VeraApp get() {
+        return getApp();
+    }
+
+    @Override
+    public List<VWidget<?>> getWidgets() {
+        return new ArrayList<>(widgets);
+    }
+
+    @Override
+    public void addWidget(VWidget<?> widget) {
+        if (widgets.contains(widget)) {
+            MinecraftVera.LOGGER.error("Can't add widget %s to compound %s, because it is already added"
+                    .formatted(widget.toString(), getClass().getSimpleName()));
+            return;
+        }
+
+        widgets.add(widget);
+        layout.addElement(widget);
+    }
+
+    @Override
+    public void removeWidget(VWidget<?> widget) {
+        if (!widgets.contains(widget)) {
+            MinecraftVera.LOGGER.error("Can't remove widget %s from compound %s, because it wasn't added"
+                    .formatted(widget.toString(), getClass().getSimpleName()));
+            return;
+        }
+
+        layout.removeElement(widget);
+        widgets.remove(widget);
+    }
+
+    //
+    // Spoofing widget positions
+    //
+
+    @Override
+    public void move(int x, int y) {
+        super.move(x, y);
+        layout.move(x, y);
+    }
+
+    @Override
+    public void setSize(int width, int height) {
+        super.setSize(width, height);
+        layout.setSize(width, height);
+    }
+
+    //
+    // Rendering
+    //
+    @Override
+    public void renderSelf() {
+        beforeRender();
+        animations.updateLifetimes();
+
+        if (visibilityConditionsPassed()) {
+            VRenderContext ctx = createRenderContext();
+            Vera.renderer.pushContext(ctx);
+
+            renderContent(ctx);
+
+            for (VWidget<?> widget : widgets) widget.renderSelf();
+
+            renderBorder(ctx);
+            renderOverlay(ctx);
+
+            Vera.renderer.popContext();
+        }
+
+        afterRender();
+    }
+}
