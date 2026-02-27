39eb5ad9c798184f756c4cbff448584a91a12023
JXSnack <jx.snack@gmail.com>
2026-02-27 19:14:19 +0100

    add no limit layout

---
+++ src/main/java/net/snackbag/vera/layout/VXLayout.java
@@ -0,0 +1,36 @@
+package net.snackbag.vera.layout;
+
+import net.snackbag.vera.VElement;
+import net.snackbag.vera.core.VAppAccess;
+import org.joml.Vector2i;
+
+public class VXLayout extends VLayout {
+    public VXLayout(VAppAccess app, int x, int y, int width, int height) {
+        super(app, x, y, width, height);
+    }
+
+    public VXLayout(VAppAccess app, int x, int y) {
+        this(app, x, y, -1, -1);
+    }
+
+    public VXLayout(VLayout parent, int width, int height) {
+        this(parent.appAccess, 0, 0, width, height);
+        this.alsoAddTo(parent);
+    }
+
+    public VXLayout(VLayout parent) {
+        this(parent, -1, -1);
+    }
+
+    @Override
+    protected Vector2i applyAlignment(Vector2i original) {
+        return original;
+    }
+
+    @Override
+    public void rebuild() {
+        for (VElement elem : elements) {
+            cache.put(elem, new Vector2i(getX() + elem.getRawX(), getY() + elem.getRawY()));
+        }
+    }
+}
