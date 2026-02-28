77822a042ae7f9ec29d78c886e842ea8756fab14
lemonnik <lemongamer6484@gmail.com>
2026-02-28 20:31:55 +0100

    GameRendererMixin versioning

--- src/main/java/net/snackbag/mcvera/mixin/GameRendererMixin.java
+++ src/main/java/net/snackbag/mcvera/mixin/GameRendererMixin.java
@@ -1,6 +1,7 @@
 package net.snackbag.mcvera.mixin;
 
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.RenderTickCounter;
 import net.snackbag.vera.InternalVera;
 import net.snackbag.vera.Vera;
 import net.snackbag.vera.flag.VAppPositioningFlag;
@@ -15,22 +16,38 @@ import java.util.List;
 @Mixin(GameRenderer.class)
 public abstract class GameRendererMixin {
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;getOverlay()Lnet/minecraft/client/gui/screen/Overlay;", ordinal = 0, shift = At.Shift.BEFORE))
-    private void mcvera$renderAboveHud(float tickDelta, long startTime, boolean tick, CallbackInfo ci) {
+    //? if (1.20.1) {
+    //private void mcvera$renderAboveHud(float tickDelta, long startTime, boolean tick, CallbackInfo ci) {
+    //? } else if (>=1.21.1) {
+    private void mcvera$renderAboveHud(RenderTickCounter tickCounter, boolean tick, CallbackInfo ci) {
+    //?}
         Vera.renderer.renderApps(VAppPositioningFlag.ABOVE_HUD);
     }
 
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;renderWithTooltip(Lnet/minecraft/client/gui/DrawContext;IIF)V"))
-    private void mcvera$renderOnGUI(float tickDelta, long startTime, boolean tick, CallbackInfo ci) {
+    //? if (1.20.1) {
+    //private void mcvera$renderAboveHud(float tickDelta, long startTime, boolean tick, CallbackInfo ci) {
+    //? } else if (>=1.21.1) {
+    private void mcvera$renderOnGUI(RenderTickCounter tickCounter, boolean tick, CallbackInfo ci) {
+    //?}
         Vera.renderer.renderApps(VAppPositioningFlag.GUI);
     }
 
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;renderWithTooltip(Lnet/minecraft/client/gui/DrawContext;IIF)V", shift = At.Shift.AFTER))
-    private void mcvera$renderAboveGUI(float tickDelta, long startTime, boolean tick, CallbackInfo ci) {
+    //? if (1.20.1) {
+    //private void mcvera$renderAboveHud(float tickDelta, long startTime, boolean tick, CallbackInfo ci) {
+    //? } else if (>=1.21.1) {
+    private void mcvera$renderAboveGUI(RenderTickCounter tickCounter, boolean tick, CallbackInfo ci) {
+    //?}
         Vera.renderer.renderApps(VAppPositioningFlag.ABOVE_GUI);
     }
 
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/toast/ToastManager;draw(Lnet/minecraft/client/gui/DrawContext;)V", shift = At.Shift.AFTER))
-    private void mcvera$renderScreenAndTop(float tickDelta, long startTime, boolean tick, CallbackInfo ci) {
+    //? if (1.20.1) {
+    //private void mcvera$renderAboveHud(float tickDelta, long startTime, boolean tick, CallbackInfo ci) {
+    //? } else if (>=1.21.1) {
+    private void mcvera$renderScreenAndTop(RenderTickCounter tickCounter, boolean tick, CallbackInfo ci) {
+    //?}
         Vera.renderer.renderApps(VAppPositioningFlag.SCREEN);
         Vera.renderer.renderApps(VAppPositioningFlag.TOP);
 
