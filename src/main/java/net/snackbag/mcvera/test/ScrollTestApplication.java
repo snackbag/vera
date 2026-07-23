2c6d1c6bc35758dc358c64a0061bea5f17732091
JXSnack <jx.snack@gmail.com>
2026-07-23 21:08:55 +0200

    move styles to stylesheet

--- src/main/java/net/snackbag/mcvera/test/ScrollTestApplication.java
+++ src/main/java/net/snackbag/mcvera/test/ScrollTestApplication.java
@@ -5,6 +5,7 @@ import net.snackbag.vera.core.VeraApp;
 import net.snackbag.vera.event.VShortcut;
 import net.snackbag.vera.layout.VXLayout;
 import net.snackbag.vera.style.VEffectState;
+import net.snackbag.vera.style.VStyleSheet;
 import net.snackbag.vera.widget.VLabel;
 import net.snackbag.vera.widget.VScrollBox;
 
@@ -14,14 +15,23 @@ public class ScrollTestApplication extends VeraApp {
     @Override
     public void init() {
         new VShortcut(this, "escape", this::hide);
+        mergeStyleSheet(createStyleSheet());
 
         VScrollBox box = new VScrollBox(this, new VXLayout(this, 0, 0), 20, 20, 100, 100).alsoAdd();
-        box.setStyle("border-size", 1);
-        box.setStyle("border-color", VEffectState.HOVERED, VColor.MC_GREEN);
-        box.setStyle("overlay", VEffectState.HOVERED, VColor.MC_WHITE.withOpacity(0.15f));
-        box.setStyle("transition", 100);
+        box.deltaYPerScroll = 3;
 
         VLabel label = new VLabel(box, "Hi there :)").alsoAdd();
         label.modifyStyleFontColor("font", VEffectState.HOVERED).rgb(VColor.MC_GOLD);
+    }
+
+    public VStyleSheet createStyleSheet() {
+        VStyleSheet sheet = new VStyleSheet();
+
+        sheet.setKey(VScrollBox.class, "border-size", 1);
+        sheet.setKey(VScrollBox.class, "border-color", VColor.MC_GREEN, VEffectState.HOVERED);
+        sheet.setKey(VScrollBox.class, "overlay", VColor.MC_WHITE.withOpacity(0.15f), VEffectState.HOVERED);
+        sheet.setKey(VScrollBox.class, "transition", 100);
+
+        return sheet;
     }
 }
