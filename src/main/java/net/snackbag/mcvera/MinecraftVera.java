72921d8e9a1a057aeda877b6af0a8b2a0b1f8932
JXSnack <jx.snack@gmail.com>
2026-07-24 19:43:52 +0200

    basic scrollbox standard style

--- src/main/java/net/snackbag/mcvera/MinecraftVera.java
+++ src/main/java/net/snackbag/mcvera/MinecraftVera.java
@@ -24,6 +24,7 @@ public class MinecraftVera implements ModInitializer {
 		Vera.registrar.registerStandardStyle(new LabelStandardStyle());
 		Vera.registrar.registerStandardStyle(new LineInputStandardStyle());
 		Vera.registrar.registerStandardStyle(new RectStandardStyle());
+		Vera.registrar.registerStandardStyle(new ScrollBoxStandardStyle());
 		Vera.registrar.registerStandardStyle(new TabWidgetStandardStyle());
 	}
 }
---
+++ src/main/java/net/snackbag/vera/style/standard/ScrollBoxStandardStyle.java
@@ -0,0 +1,33 @@
+package net.snackbag.vera.style.standard;
+
+import net.snackbag.vera.InternalVera;
+import net.snackbag.vera.core.VColor;
+import net.snackbag.vera.core.VImage;
+import net.snackbag.vera.style.StyleValueType;
+import net.snackbag.vera.style.VStyleSheet;
+import net.snackbag.vera.widget.VScrollBox;
+
+public class ScrollBoxStandardStyle implements VStandardStyle {
+    @Override
+    public void apply(VStyleSheet sheet) {
+        sheet.setKey(VScrollBox.class, "border-size", 1);
+        sheet.setKey(VScrollBox.class, "background", VColor.white());
+
+        sheet.setKey(VScrollBox.class, "scroll-increment", new VImage(InternalVera.id("widgets/scrollbox/increment.png")));
+        sheet.setKey(VScrollBox.class, "scroll-thumb", new VImage(InternalVera.id("widgets/scrollbox/thumb.png")));
+        sheet.setKey(VScrollBox.class, "scroll-track", VColor.MC_GRAY);
+        sheet.setKey(VScrollBox.class, "scroll-decrement", new VImage(InternalVera.id("widgets/scrollbox/decrement.png")));
+        sheet.setKey(VScrollBox.class, "scroll-width", 16);
+    }
+
+    @Override
+    public void reserve(VStyleSheet sheet) {
+        sheet.reserveType("background", StyleValueType.FILL);
+
+        sheet.reserveType("scroll-increment", StyleValueType.FILL);
+        sheet.reserveType("scroll-thumb", StyleValueType.FILL);
+        sheet.reserveType("scroll-track", StyleValueType.FILL);
+        sheet.reserveType("scroll-decrement", StyleValueType.FILL);
+        sheet.reserveType("scroll-width", StyleValueType.INT);
+    }
+}
