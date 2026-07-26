4fb5c8f26f8230e201ec36816f37e2f9cd1532da
JXSnack <jx.snack@gmail.com>
2026-07-26 13:06:04 +0200

    remove redundant effect states

--- src/main/java/net/snackbag/vera/style/VEffectState.java
+++ src/main/java/net/snackbag/vera/style/VEffectState.java
@@ -12,22 +12,8 @@ public enum VEffectState {
     RIGHT_CLICKED("right-click", CLICKED),
 
     LC_DRAGGING("lc-drag", LEFT_CLICKED),
-    LC_DRAG_TOP("lc-drag-top", LC_DRAGGING),
-    LC_DRAG_BOTTOM("lc-drag-bottom", LC_DRAGGING),
-    LC_DRAG_LEFT("lc-drag-left", LC_DRAGGING),
-    LC_DRAG_RIGHT("lc-drag-right", LC_DRAGGING),
-
     MC_DRAGGING("mc-drag", MIDDLE_CLICKED),
-    MC_DRAG_TOP("mc-drag-top", MC_DRAGGING),
-    MC_DRAG_BOTTOM("mc-drag-bottom", MC_DRAGGING),
-    MC_DRAG_LEFT("mc-drag-left", MC_DRAGGING),
-    MC_DRAG_RIGHT("mc-drag-right", MC_DRAGGING),
-
-    RC_DRAGGING("rc-drag", RIGHT_CLICKED),
-    RC_DRAG_TOP("rc-drag-top", RC_DRAGGING),
-    RC_DRAG_BOTTOM("rc-drag-bottom", RC_DRAGGING),
-    RC_DRAG_LEFT("rc-drag-left", RC_DRAGGING),
-    RC_DRAG_RIGHT("rc-drag-right", RC_DRAGGING);
+    RC_DRAGGING("rc-drag", RIGHT_CLICKED);
 
     public final String identifier;
     public final @Nullable VEffectState fallback;
